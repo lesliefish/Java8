a018799f650a91d9d192639e2c0a4e79b25e3b3d
leiyu <yuleics@outlook.com>
2019-03-08 11:00:03 +0800

    java8 test01

---
+++ src/com/lesliefish/Main.java
@@ -0,0 +1,9 @@
+package com.lesliefish;
+import com.lesliefish.test01.Java8Tester;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Java8Tester.test();
+    }
+}
---
+++ src/com/lesliefish/test01/Java8Tester.java
@@ -0,0 +1,51 @@
+package com.lesliefish.test01;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Java8Tester {
+    // java7排序
+    private void sortUsingJava7(List<String> names) {
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+    }
+
+    // java8排序
+    private void sortUsingJava8(List<String> names){
+        Collections.sort(names,(s1, s2) -> s1.compareTo(s2));
+    }
+
+
+    public static void test() {
+        List<String> names1 = new ArrayList<String>();
+        names1.add("Mahesh ");
+        names1.add("Suresh ");
+        names1.add("Ramesh ");
+        names1.add("Naresh ");
+        names1.add("Kalpesh ");
+
+        List<String> names2 = new ArrayList<String>();
+        names2.add("Mahesh ");
+        names2.add("Suresh ");
+        names2.add("Ramesh ");
+        names2.add("Naresh ");
+        names2.add("Kalpesh ");
+
+
+        Java8Tester tester = new Java8Tester();
+        System.out.println("Sort using Java 7 syntax: ");
+
+        tester.sortUsingJava7(names1);
+        System.out.println(names1);
+        System.out.println("Sort using Java 8 syntax: ");
+
+        tester.sortUsingJava8(names2);
+        System.out.println(names2);
+    }
+}
