b2a243e4485a3dca113d78f45227708c2f72bbb2
leiyu <yuleics@outlook.com>
2019-03-08 15:01:11 +0800

    java8 test04 -- Functional Interface

--- src/com/lesliefish/Main.java
+++ src/com/lesliefish/Main.java
@@ -3,12 +3,15 @@ import com.lesliefish.test01overview.Java8TesterOverView;
 import com.lesliefish.test02lambdaexpressions.LambdaTester01;
 import com.lesliefish.test02lambdaexpressions.LambdaTester02;
 import com.lesliefish.test03methodreferences.MethodReferencesTester;
+import com.lesliefish.test04functionalinterface.FunctionalInterface;
+
 public class Main {
 
     public static void main(String[] args) {
         // Java8TesterOverView.test();
         //LambdaTester01.test();
         //LambdaTester02.test();
-        MethodReferencesTester.test();
+        //MethodReferencesTester.test();
+        FunctionalInterface.test();
     }
 }
---
+++ src/com/lesliefish/test04functionalinterface/FunctionalInterface.java
@@ -0,0 +1,29 @@
+package com.lesliefish.test04functionalinterface;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FunctionalInterface {
+    public static void test() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        System.out.println("print all numbers :");
+        eval(list, n -> true);
+
+        System.out.println("Print even numbers:"); // 偶数
+        eval(list, n -> n % 2 == 0);
+
+        System.out.println("Print numbers greater than 3:"); // 大于3的数
+        eval(list, n-> n > 3 );
+    }
+
+
+    public static void eval(List<Integer> list, Predicate<Integer> predicate) {
+        for (Integer n : list) {
+            if (predicate.test(n)) {
+                System.out.println(n);
+            }
+        }
+    }
+}
